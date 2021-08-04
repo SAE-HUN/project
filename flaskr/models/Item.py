ea55a57584480149e095f61055de3ca0c9a6a846
SAEHUN <devhun58@gmail.com>
2021-08-04 22:02:18 +0900

    Rename flaskr folder to app

--- flaskr/models/Item.py
+++
@@ -1,7 +0,0 @@
-from . import db
-
-class Item(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(32), nullable=False)
-    price = db.Column(db.Integer, nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
