3f670a05c422d726bfdbe26af8ea66ff37dc7bba
SAEHUN <devhun58@gmail.com>
2021-08-04 16:47:18 +0900

    Add __init__ function
    User and shop is related as one to one. So when user is created, shop also should be
    create. __init__ function solve that problem.

--- flaskr/models/User.py
+++ flaskr/models/User.py
@@ -9,3 +9,8 @@ class User(db.Model):
     money = db.Column(db.Integer, default=0, nullable=False)
     shop = db.relationship(Shop, backref='user', uselist=False)
     items = db.relationship(Item, backref='user')
+    
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+        self.shop = Shop()
