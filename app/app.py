42c9cb1aecb87f081b627a3091225a7445c7d6e3
SAEHUN <devhun58@gmail.com>
2021-08-10 16:44:19 +0900

    Add app.py
    Make flask application and configure it with environment variable
    'APP_CONFIG_FILE'. And socketio, jwtmanager, database initialized with
    this app. Finally, socketio run with it.

---
+++ app/app.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from flask_socketio import SocketIO
+from flask_jwt_extended import JWTManager
+
+from models import db
+from blueprints.auth import auth
+from controllers.shop import Shop as ns_shop
+
+app = Flask(__name__)
+socketio = SocketIO(app, cors_allowed_origins="*")
+jwt = JWTManager(app)
+db.init_app(app)
+
+app.config.from_envvar('APP_CONFIG_FILE')
+
+with app.app_context():
+    from models import user, shop, item
+    db.create_all()
+
+app.register_blueprint(auth)
+socketio.on_namespace(ns_shop('/shops'))
+
+if __name__ == '__main__':
+    socketio.run(app)
