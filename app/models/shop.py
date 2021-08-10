305b8c2dff91ca682e085b2e157ea7f7b2b31628
SAEHUN <devhun58@gmail.com>
2021-08-10 16:57:47 +0900

    Update shop.py
    Change column open to is_open and Add function get_opened_shops, get,
    open, close that is used in controllers.

--- app/models/shop.py
+++ app/models/shop.py
@@ -1,6 +1,22 @@
-from . import db
+from . import db, commit
 
 class Shop(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    open = db.Column(db.Boolean, default=False, nullable=False)
+    is_open = db.Column(db.Boolean, default=False, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def get_opend_shops():
+        shops = Shop.query.filter_by(is_open=True).all()
+        return shops
+    
+    def get(shop_id):
+        shop = Shop.query.get(shop_id)
+        return shop
+    
+    def open(self):
+        self.is_open = True
+        commit(self)
+    
+    def close(self):
+        self.is_open = False
+        commit(self)
