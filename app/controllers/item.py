7c4554eacadf4bd9fce4b0f5d846304b67859ee1
SAE-HUN <devhun58@gmail.com>
2021-08-11 16:21:15 +0900

    Update item.py
    I implemented on_buy function that recieve item data and let user buy
    the item. If result is success emit buy event to shop that item was in,
    and else emit buy event with reason.

--- app/controllers/item.py
+++ app/controllers/item.py
@@ -1,7 +1,9 @@
 from flask_socketio import Namespace, emit
+from flask_jwt_extended import get_jwt_identity, jwt_required
 
 from models import serialize
 from models.item import Item as md_Item
+from models.user import User
 
 class Item(Namespace):
     def on_connect(self):
@@ -12,3 +14,15 @@ class Item(Namespace):
         items = md_Item.get_items(room)
         items_to_json = [serialize(item) for item in items]
         emit('get_items', {'items': items_to_json})
+    
+    @jwt_required()
+    def on_buy(self, data):
+        buyer_username = get_jwt_identity()
+        item = data['item']
+        buyer = User.get(username=buyer_username)
+        result = buyer.buy(item)
+        
+        if result['result'] == 'success':
+            emit('buy', {'result': 'success', 'item': item}, namespace='/shops', to=result['seller'])
+        else:
+            emit('buy', {'result': 'fail', 'reason': result['reason']})
