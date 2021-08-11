f848f6e2b03368d4e48d3c1a2cff566205b13f89
SAE-HUN <devhun58@gmail.com>
2021-08-11 16:06:29 +0900

    Update item.py
    I implemented on_get_items function that recieve room value and return
    item list of room.

--- app/controllers/item.py
+++ app/controllers/item.py
@@ -1,5 +1,14 @@
 from flask_socketio import Namespace, emit
 
+from models import serialize
+from models.item import Item as md_Item
+
 class Item(Namespace):
     def on_connect(self):
         emit('connect', {'result': 'success'})
+    
+    def on_get_items(self, data):
+        room = data['room']
+        items = md_Item.get_items(room)
+        items_to_json = [serialize(item) for item in items]
+        emit('get_items', {'items': items_to_json})
