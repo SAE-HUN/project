3257abe8010850f80a828be26a8f32aa82f2456c
SAE-HUN <devhun58@gmail.com>
2021-08-11 14:41:47 +0900

    Update item.py
    Implement create function that initialize item instance and commit it.

--- app/models/item.py
+++ app/models/item.py
@@ -1,7 +1,12 @@
-from . import db
+from . import db, commit
 
 class Item(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(32), nullable=False)
     price = db.Column(db.Integer, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def create(name, price, user_id):
+        item = Item(name=name, price=price, user_id=user_id)
+        commit(item)
+        return item
