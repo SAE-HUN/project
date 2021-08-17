0ecbeb6b90a2eccf85084247749ca7dbe0889063
SAE-HUN <devhun58@gmail.com>
2021-08-17 19:37:42 +0900

    Add app\controllers\market.py

---
+++ app/controllers/market.py
@@ -0,0 +1,33 @@
+from flask_socketio import Namespace, emit
+from flask_jwt_extended import jwt_required
+from flask_jwt_extended import get_jwt_identity
+
+from modules.errors import *
+from modules.handlers import handle_socket_error
+from models.user import User
+from models.store import Store
+
+
+class Market(Namespace):
+    @jwt_required()
+    def on_open(self, data):
+        nickname = get_jwt_identity()
+        user = User.query.filter_by(nickname=nickname).first()
+
+        try:
+            name = data["name"]
+            description = data.get("description", "")
+        except KeyError:
+            raise NO_REQUIRED_PARAMS()
+
+        try:
+            store = Store.create(name, description, user.id)
+        except:
+            raise SERVER_ERROR()
+
+        response = {
+            "result": False,
+            "store": {"id": store.id, "name": name, "description": description},
+            "user": {"nickname": nickname},
+        }
+        emit("open", response, namespace="/market")
