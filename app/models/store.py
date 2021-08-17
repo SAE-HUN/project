4f70e689e236c07625d50b9171bba922e187f58c
SAE-HUN <devhun58@gmail.com>
2021-08-17 19:38:08 +0900

    Add app\models\store.py to use Store table

---
+++ app/models/store.py
@@ -0,0 +1,15 @@
+from . import db
+
+
+class Store(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), nullable=False)
+    description = db.Column(db.String(256))
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+
+    def create(name, description, user_id):
+        store = Store(name=name, description=description, user_id=user_id)
+        db.session.add(store)
+        db.session.commit()
+
+        return store
