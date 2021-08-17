b6f696c43ca279b058040cdcad05f08c9a0113b5
SAE-HUN <devhun58@gmail.com>
2021-08-17 19:30:31 +0900

    Update app\app.py to use socket

--- app/app.py
+++ app/app.py
@@ -1,13 +1,21 @@
-from flask import Flask
+from flask import Flask, request
 
 from flask_jwt_extended import JWTManager
+from flask_socketio import SocketIO, emit
 
 from blueprints import user
+from controllers.market import Market
 from models import db
 from models.user import User
+from modules.handlers import handle_socket_error
 
 app = Flask(__name__)
 jwt = JWTManager(app)
+socketio = SocketIO(app, cors_allowed_origins="*")
+
+
+socketio.on_error("/market")(handle_socket_error)
+
 
 app.config["SECRET_KEY"] = "1234"
 app.config[
@@ -15,11 +23,13 @@ app.config[
 ] = "sqlite:///C:/Users/fjdks/project/app/db.sqlite3"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SQLALCHEMY_COMMIT_ON_TEARDOWN"] = True
+
 app.register_blueprint(user.bp)
+socketio.on_namespace(Market("/market"))
 
 db.init_app(app)
 with app.app_context():
     db.create_all()
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    socketio.run(app=app, debug=True)
