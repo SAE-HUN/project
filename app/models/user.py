71fb5d33d975aa40a220690a489ffa6c847669aa
SAE-HUN <devhun58@gmail.com>
2021-08-17 19:38:47 +0900

    Update app\models\user.py to add store column

--- app/models/user.py
+++ app/models/user.py
@@ -1,6 +1,7 @@
 from werkzeug.security import generate_password_hash
 
 from . import db
+from models.store import Store
 
 
 class User(db.Model):
@@ -9,6 +10,7 @@ class User(db.Model):
     password = db.Column(db.String(128), nullable=False)
     nickname = db.Column(db.String(32), unique=True, nullable=False)
     money = db.Column(db.Integer, default=0)
+    store = db.relationship(Store, backref="user", lazy=True)
 
     def create(username, password, nickname):
         user = User(
