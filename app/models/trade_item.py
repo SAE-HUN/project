6f1e740c4fb4446a279518d09af1bde29a953ac0
SAE-HUN <devhun58@gmail.com>
2021-08-18 23:49:23 +0900

    Add app\models\trade_item.py

---
+++ app/models/trade_item.py
@@ -0,0 +1,25 @@
+from . import db
+
+
+class TradeItem(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(32), nullable=False)
+    description = db.Column(db.String(64))
+    category = db.Column(db.String(32), nullable=False)
+    price = db.Column(db.Integer, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    store_id = db.Column(db.Integer, db.ForeignKey("store.id"), nullable=False)
+
+    def create(name, description, category, price, user_id, store_id):
+        item = TradeItem(
+            name=name,
+            description=description,
+            category=category,
+            price=price,
+            user_id=user_id,
+            store_id=store_id,
+        )
+        db.session.add(item)
+        db.session.commit()
+
+        return item
