ada2075275efcd7dd9dc1235301c05832b2ac205
SAE-HUN <devhun58@gmail.com>
2021-08-18 23:48:28 +0900

    Update app\app.py

--- app/app.py
+++ app/app.py
@@ -5,8 +5,8 @@ from flask_socketio import SocketIO, emit
 
 from blueprints import user
 from controllers.market import Market
+from controllers.store import Store
 from models import db
-from models.user import User
 from modules.handlers import handle_socket_error
 
 app = Flask(__name__)
@@ -15,6 +15,7 @@ socketio = SocketIO(app, cors_allowed_origins="*")
 
 
 socketio.on_error("/market")(handle_socket_error)
+socketio.on_error("/store")(handle_socket_error)
 
 
 app.config["SECRET_KEY"] = "1234"
@@ -26,6 +27,7 @@ app.config["SQLALCHEMY_COMMIT_ON_TEARDOWN"] = True
 
 app.register_blueprint(user.bp)
 socketio.on_namespace(Market("/market"))
+socketio.on_namespace(Store("/store"))
 
 db.init_app(app)
 with app.app_context():
